54d20246b7c90a9e264ea9750f824f705cda398f
DevWeb13 <giuliano.loic@sfr.fr>
2023-06-19 10:44:44 +0200

    feat: (index.ts): Create class Grid.
    Create class Grid + style.css + displays instructions

--- index.js
+++ index.js
@@ -1,2 +1,38 @@
-const gridXInput = document.getElementById('gridX'), gridYInput = document.getElementById('gridY'), positionXInput = document.getElementById('positionX'), positionYInput = document.getElementById('positionY'), directionInput = document.getElementById('direction'), gridContainer = document.getElementById('gridContainer'), instructionsInput = document.getElementById('instructions');
-console.log(gridXInput, gridYInput, positionXInput, positionYInput, directionInput, gridContainer, instructionsInput);
+const form = document.getElementById('form'), gridXInput = document.getElementById('gridX'), gridYInput = document.getElementById('gridY'), positionXInput = document.getElementById('positionX'), positionYInput = document.getElementById('positionY'), directionInput = document.getElementById('direction'), gridContainer = document.getElementById('gridContainer'), instructionsInput = document.getElementById('instructions'), submitButton = document.getElementById('submit');
+console.log(gridXInput, gridYInput, positionXInput, positionYInput, directionInput, gridContainer, instructionsInput, submitButton);
+class Grid {
+    constructor(container, x, y) {
+        this.container = container;
+        this.x = x;
+        this.y = y;
+        this.init();
+        this.display();
+    }
+    init() {
+        this.container.innerHTML = '';
+        for (let i = 0; i < this.x; i++) {
+            const row = document.createElement('div');
+            row.className = 'row';
+            for (let j = this.y - 1; j >= 0; j--) {
+                const cell = document.createElement('div');
+                cell.className = 'cell';
+                cell.innerHTML = 'x:' + i.toString() + ' ' + 'y:' + j.toString();
+                row.appendChild(cell);
+            }
+            this.container.appendChild(row);
+        }
+    }
+    reset() {
+        this.container.innerHTML = '';
+    }
+    display() {
+        return this.container;
+    }
+}
+const start = (e) => {
+    e.preventDefault();
+    const gridX = parseInt(gridXInput.value);
+    const gridY = parseInt(gridYInput.value);
+    new Grid(gridContainer, gridX, gridY);
+};
+form.addEventListener('submit', start);
--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
-const gridXInput = document.getElementById('gridX') as HTMLInputElement,
+const form = document.getElementById('form') as HTMLFormElement,
+  gridXInput = document.getElementById('gridX') as HTMLInputElement,
   gridYInput = document.getElementById('gridY') as HTMLInputElement,
   positionXInput = document.getElementById('positionX') as HTMLInputElement,
   positionYInput = document.getElementById('positionY') as HTMLInputElement,
@@ -6,7 +7,8 @@ const gridXInput = document.getElementById('gridX') as HTMLInputElement,
   gridContainer = document.getElementById('gridContainer'),
   instructionsInput = document.getElementById(
     'instructions'
-  ) as HTMLInputElement;
+  ) as HTMLInputElement,
+  submitButton = document.getElementById('submit') as HTMLButtonElement;
 
 console.log(
   gridXInput,
@@ -15,5 +17,49 @@ console.log(
   positionYInput,
   directionInput,
   gridContainer,
-  instructionsInput
+  instructionsInput,
+  submitButton
 );
+
+class Grid {
+  constructor(
+    private container: HTMLElement,
+    private x: number,
+    private y: number
+  ) {
+    this.init();
+    this.display();
+  }
+
+  init() {
+    this.container.innerHTML = '';
+    for (let i = 0; i < this.x; i++) {
+      const row = document.createElement('div');
+      row.className = 'row';
+      for (let j = this.y - 1; j >= 0; j--) {
+        const cell = document.createElement('div');
+        cell.className = 'cell';
+        cell.innerHTML = 'x:' + i.toString() + ' ' + 'y:' + j.toString();
+        row.appendChild(cell);
+      }
+      this.container.appendChild(row);
+    }
+  }
+
+  reset() {
+    this.container.innerHTML = '';
+  }
+
+  display() {
+    return this.container;
+  }
+}
+
+const start = (e) => {
+  e.preventDefault();
+  const gridX = parseInt(gridXInput.value);
+  const gridY = parseInt(gridYInput.value);
+  new Grid(gridContainer, gridX, gridY);
+};
+
+form.addEventListener('submit', start);
